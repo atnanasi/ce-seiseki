639514d58c64124956527d2e1d8439e37984ca09
Atnanasi <ys2000pro@gmail.com>
2016-10-26 10:39:07 +0900

    Init rails file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seiseki::Application.config.secret_token = 'e1e4ef66fc2c390294dc3aee24ae1c15f4f1db0e242d2b8cfa512ab288b67e0db9b013f3988779525c36a5a62c0ec706e821dda09392f07fae1853d9589f99c2'
